b2f2ac986a7b57bf2cd9b86f7cdda7de78cd752b
nnjnuinijnnj <abzartajhiz@gmail.com>
2026-07-17 23:27:05 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+fetch("movies.json")
+.then(response => response.json())
+.then(movies => {
+
+const box = document.getElementById("movies");
+
+
+movies.forEach(movie => {
+
+box.innerHTML += `
+
+<div class="card">
+
+<img src="${movie.poster}">
+
+<h3>${movie.title}</h3>
+
+<button onclick="openMovie(${movie.id})">
+مشاهده
+</button>
+
+</div>
+
+`;
+
+});
+
+
+});
+
+
+function openMovie(id){
+
+window.location.href = "movie.html?id=" + id;
+
+}
